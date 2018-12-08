9b9b62116ccd3600701d89a2ac24781efb344a63
jlscire <45699755+jlscire@users.noreply.github.com>
2018-12-08 00:00:37 -0400

    Add files via upload

--- planillaregistro.php
+++ planillaregistro.php
@@ -28,14 +28,13 @@
             <div class="user-section"><!--user image section-->
                     <div class="user-section-inner">
                     <img src="assets/img/iutoms.jpg" alt="">
-            </li>
-            </div>
+                    </div>
             <div class="user-info">
                    <li class="dropdown">
                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                <div>Eikel<strong> Guariguata</strong></div></a>
+                <div><strong>Eikel Guariguata</strong></div></a>
                 <ul class="dropdown-menu dropdown-user"><!-- dropdown user-->
-                    <a href="logout.php"><i class="fa fa-users fa-fw"></i><b>Crear Usuario</b></a>
+                        <li><a href="logout.php"><i class="fa fa-users fa-fw"></i>Crear Usuario</a></li>
                         <li class="divider"></li>
                         <li><a href="logout.php"><i class="fa fa-gear fa-fw"></i>Configuración de Usuario</a></li>
                         <li class="divider"></li>
@@ -72,13 +71,13 @@
         </nav><!-- end navbar side -->
         <div id="page-wrapper"><!--  page-wrapper -->
             <div class="row"><!-- Page Header -->
-                <div class="col-lg-12">
+            <div class="col-lg-12"><!-- col-12 -->
                     <h1 class="page-header">Sistema de Registro de Pre-Inscripción</h1>
                     
-                    <form action="assets\includes\IndexAjax.php" role="form"> -->
-     <strong>Datos Alumnos</strong>
-        <br>
-        <br>
+    <form action="assets\includes\IndexAjax.php" role="form"> 
+            <strong>Datos Alumnos</strong>
+                    <br>
+                    <br>
             <div class="row form-group">
                 <div class="col col-md-4">
                     <label for="text-input" class=" form-control-label">Nombres</label>
@@ -88,13 +87,13 @@
                     <label for="text-input" class=" form-control-label">Apellidos</label>
                     <input type="text" id="apellidos" class="form-control">
                 </div>
-                 <div class="col col-md-4">
+                <div class="col col-md-4">
                     <label for="text-input" class=" form-control-label">Cedula Escolar</label>
                     <input type="text" id="cedulaescolar" class="form-control">
                 </div>   
             </div> 
             <div class="row form-group">
-                 <div class="col col-md-3">
+                <div class="col col-md-3">
                     <label for="text-input" class=" form-control-label">Lugar de Nacimiento</label>
                     <select name="select" id="lugar_nac" class="form-control">
                         <option value="0">Seleccione</option>           
@@ -109,24 +108,24 @@
                     <input type="text" id="edad_alumno" class="form-control">   
                 </div>
                 <div class="col col-md-3">
-                    <label for="text-input" class=" form-control-label">NÂ° TelÃ©fono</label>
+                    <label for="text-input" class=" form-control-label">N° de Teléfono</label>
                     <input type="text" id="telefono_alumno" class="form-control">   
                 </div>
             </div>
-             <div class="row form-group">
+            <div class="row form-group">
                 <div class="col col-md-12">
-                    <label for="text-input" class=" form-control-label">DirecciÃ³n</label>
+                    <label for="text-input" class=" form-control-label">Dirección</label>
                     <input type="text" id="direccion_alumno" class="form-control">   
                 </div>            
             </div> 
-        </div>
-        <div class="card-body card-block">
+            </div><!-- end col-12 -->
+            <div class="card-body card-block">
             <div class="card-header">
                 <br>
-            <strong>Datos</strong> Familiares
-        </div>
-        <br>
-         <div class="row form-group">
+            <strong>Datos Familiares</strong> 
+            </div>
+                <br>
+            <div class="row form-group">
                 <div class="col col-md-6">
                     <label for="text-input" class=" form-control-label">Nombres y Apellidos de la Madre</label>
                     <input type="text" id="nombre_madre" class="form-control">   
@@ -136,17 +135,17 @@
                     <input type="text" id="ci_madre" class="form-control">   
                 </div>
             </div>
-         <div class="row form-group">
+            <div class="row form-group">
                 <div class="col col-md-8">
                     <label for="text-input" class=" form-control-label">Empresa Donde Trabaja</label>
                     <input type="text" id="empresa_madre" class="form-control">   
                 </div>
                 <div class="col col-md-4">
-                    <label for="text-input" class=" form-control-label">NÂ° TelÃ©fono</label>
+                    <label for="text-input" class=" form-control-label">N° de Teléfono</label>
                     <input type="text" id="telefono_madre" class="form-control">   
                 </div>
             </div>
-             <div class="row form-group">
+            <div class="row form-group">
                 <div class="col col-md-6">
                     <label for="text-input" class=" form-control-label">Nombres y Apellidos del Padre</label>
                     <input type="text" id="nombre_padre" class="form-control">   
@@ -156,22 +155,22 @@
                     <input type="text" id="ci_padre" class="form-control">   
                 </div>    
             </div>
-             <div class="row form-group">
+            <div class="row form-group">
                 <div class="col col-md-8">
                     <label for="text-input" class=" form-control-label">Empresa Donde Trabaja</label>
                     <input type="text" id="empresa_padre" class="form-control">   
                 </div>
                 <div class="col col-md-4">
-                    <label for="text-input" class=" form-control-label">NÂ° TelÃ©fono</label>
+                    <label for="text-input" class=" form-control-label">N° de Telefono</label>
                     <input type="text" id="telefono_padre" class="form-control">   
                 </div>
             </div>
 
-        </div>
-        <div class="card-body card-block">
+            </div><!-- end card-body -->
+            <div class="card-body card-block">
             <div class="card-header">
                 <br>
-            <strong>Datos del</strong> Representante Legal
+            <strong>Datos del Representante Legal</strong> 
             </div>
             <br>
             <div class="row form-group">
@@ -192,13 +191,13 @@
             </div>        
             <div class="row form-group">
                 <div class="col col-md-12">
-                    <label for="text-input" class=" form-control-label">Direccion</label>
+                    <label for="text-input" class=" form-control-label">Dirección</label>
                     <input type="text" id="direc_representante" class="form-control">   
                 </div>            
             </div>
             <div class="row form-group">
                 <div class="col col-md-4">
-                    <label for="text-input" class=" form-control-label">Profecion</label>
+                    <label for="text-input" class=" form-control-label">Profesión</label>
                     <input type="text" id="profecion" class="form-control">   
                 </div>
                 <div class="col col-md-4">
@@ -206,11 +205,12 @@
                     <input type="text" id="empre_repres" class="form-control">   
                 </div>
                 <div class="col col-md-4">
-                    <label for="text-input" class=" form-control-label">Numero Telefonico</label>
+                    <label for="text-input" class=" form-control-label">Número Telefonico</label>
                     <input type="text" id="tele_repre" class="form-control">   
-                    </form>
                 </div>
-            </div>
+            </div> 
+            <button type="submit" class="btn btn-primary">Registrar</button>
+        </form>
         </div>
     </div> 
                 </div>
