e3b02ef0eb8afd56336ea626d77747919152e2d2
jlscire <45699755+jlscire@users.noreply.github.com>
2018-12-09 12:29:23 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -11,53 +11,52 @@ session_start();
     <title>SIGANE | Inicio Sesión</title>
     <!-- Core CSS - Include with every page -->
     <link href="assets/plugins/bootstrap/bootstrap.css" rel="stylesheet" />
+    <!--  <link rel="stylesheet" href="assets/css/compiled-4.5.15.min.css" /> -->
     <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
     <link href="assets/plugins/pace/pace-theme-big-counter.css" rel="stylesheet" />
-   <link href="assets/css/style.css" rel="stylesheet" />
-      <link href="assets/css/main-style.css" rel="stylesheet" />
+    <link href="assets/css/floating-labels.css" rel="stylesheet"/>
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <link href="assets/css/main-style.css" rel="stylesheet" />
+      <!-- Custom styles for this template -->   
 
 </head>
 
 <body class="body-Login-back">
+    
 
     <div class="container">
        
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4 text-center logo-margin ">
-              <img src="assets/img/Sigane2.png" alt=""/>
-                </div>
-            <div class="col-md-4 col-md-offset-4">
-                <div class="login-panel panel panel-default">                  
-                    <div class="panel-heading">
-                        <h3 class="text-center panel-title"><b>Ingrese sus datos<b></h3>
-                    </div>
-                    <div class="panel-body">
-                        <!-- <form action="assets\includes\IndexAjax.php" role="form"> -->
-                            <fieldset>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Username" name="username" type="text" autofocus id="username">
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Password" name="password" type="password" value="" id ="password">
-                                </div>
-                                    <div class="checkbox">
-                                        <label>
-                                            <input name="remember" type="checkbox" value="Remember Me">Recordar usuario
-                                        </label>
-                                    </div> 
-                                <button type="submit" class="btn btn-lg btn-success btn-block" id="Entrar">Entrar</button>
-                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="mensaje">
-                                </div>
-                            </fieldset>
-                        <!-- </form> -->
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+       <div class="row">
+            
+     <form class="form-signin border">
+      <div class="text-center mb-4">
+        <img class="mb-4" src="../../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+        <h1 class="h3 mb-3 font-weight-normal">Ingrese sus datos</h1>       
+      </div>
+
+      <div class="form-label-group">
+        <label for="username">Usuario</label>
+        <input type="text" id="username" class="form-control" placeholder="Usuario" required autofocus>
+      </div>
+
+      <div class="form-label-group">      
+        <label for="password">Password</label>
+        <input type="password" id="password" class="form-control" value="" placeholder="Password" required>
+      </div>
+
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Recordar usuario
+        </label>
+      </div>
+      <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
+      
+     </form> 
+    </div></div>
+  
 
      <!-- Core Scripts - Include with every page -->
-    <script src="assets/plugins/jquery-1.10.2.js"></script>
+    <script src="assets/plugins/js/jquery-3.3.1.min.js"></script>
     <script src="assets/plugins/bootstrap/bootstrap.min.js"></script>
     <script src="assets/scripts/Login.js"></script>
     <script src="assets/plugins/metisMenu/jquery.metisMenu.js"></script>
