b1765dacb2b5bde21914fa0bca2ee347a050cae8
E.O <44305384+eguariguata@users.noreply.github.com>
2018-12-13 19:24:43 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="assets\img\registro.png">
     <title>SIGANE | Inicio Sesión</title>
     <!-- Core CSS - Include with every page -->
     <link href="assets/plugins/bootstrap/bootstrap.css" rel="stylesheet" />
@@ -24,37 +25,36 @@ session_start();
 
     <div class="container">
 
-      <div class="py-5 text-center">
+        <div class="py-5 text-center">
   
         <img src="assets/img/Sigane2.png" alt="" />
        
-      </div>
-
-      <div class="row justify-content-center">
-        <div class="col-4">
-                      
-            <div class="panel-body">
-              
-            <fieldset>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Username" name="username" type="text" autofocus id="username">
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Password" name="password" type="password" value="" id ="password">
-                                </div>
-                                    <div class="checkbox">
-                                        <label>
-                                            <input name="remember" type="checkbox" value="Remember Me">Recordar usuario
-                                        </label>
-                                    </div> 
-                                <button type="submit" class="btn btn-lg btn-success btn-block" id="Entrar">Entrar</button>
-                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="mensaje">
-                                </div>
-                            </fieldset>
-            </div>
-         
         </div>
-      </div>  
+
+        <div class="row justify-content-center">
+            <div class="col-4">
+                      
+                <div class="panel-body">
+                  
+                    <fieldset>
+                        <div class="form-group">
+                            <input class="form-control" placeholder="Username" name="username" type="text" autofocus id="username">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" placeholder="Password" name="password" type="password" value="" id ="password">
+                        </div>
+                            <div class="checkbox">
+                                <label>
+                                    <input name="remember" type="checkbox" value="Remember Me">Recordar usuario
+                                </label>
+                            </div> 
+                        <button type="submit" class="btn btn-lg btn-success btn-block" id="Entrar">Entrar</button>
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="mensaje">
+                        </div>
+                    </fieldset>
+                </div>
+            </div>
+        </div>  
     </div>    
      <!-- Core Scripts - Include with every page -->
     <script src="assets/plugins/js/jquery-3.3.1.min.js"></script>
